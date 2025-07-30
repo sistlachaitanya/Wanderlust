aabfe066354bcaf3e4b528c41f139abcb6791957
Chaitanya <sssvchaitanya@gmail.com>
2025-07-30 18:48:36 +0530

    reviews setup for listings

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const listingSchema = require("./schema.js");
 const { error } = require("console");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -113,6 +114,21 @@ app.delete(
     res.redirect("/listings");
   })
 );
+
+//Reviews
+//Post route
+
+app.post("/listings/:id/reviews", async (req, res) => {
+  const { review } = req.body;
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(review);
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  res.redirect(`/listings/${listing._id}`);
+});
 
 app.all(/.*/, (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
