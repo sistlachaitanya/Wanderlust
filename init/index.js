970e000d66fcc6a4b2844e5b3df2756dcd5ecc4b
Chaitanya <sssvchaitanya@gmail.com>
2025-08-11 14:01:35 +0530

    associate owner with listing

--- init/index.js
+++ init/index.js
@@ -2,24 +2,28 @@ const mongoose = require("mongoose");
 const initData = require("./data");
 const Listings = require("../models/listing");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-    .then(() => {
-        console.log("Database Connection Successful");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .then(() => {
+    console.log("Database Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDb = async () => {
-    await Listings.deleteMany({});
-    await Listings.insertMany(initData.data);
-    console.log("Data was Initialized");
-}
+  await Listings.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "689996cb8f2b363eebd1a19f",
+  }));
+  await Listings.insertMany(initData.data);
+  console.log("Data was Initialized");
+};
 
 initDb();
--- routes/listing.js
+++ routes/listing.js
@@ -35,11 +35,14 @@ router.get(
   "/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id)
+      .populate("reviews")
+      .populate("owner");
     if (!listing) {
       req.flash("error", "Listing you requested doesn't exist!");
       return res.redirect("/listings");
     }
+    console.log(listing);
     res.render("listings/show.ejs", { listing });
   })
 );
@@ -51,6 +54,7 @@ router.post(
   validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
